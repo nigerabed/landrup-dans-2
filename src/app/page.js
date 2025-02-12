f26df5eca30a101d77b8c3bc7a30dbffe027d326
nigerabed <nigerabed@gmail.com>
2025-02-12 01:11:22 +0100

    rename logo

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import logo from "../assets/logo.png";
+import logo from "../assets/Logo.png";
 import Button from "@/components/Button";
 import Link from "next/link";
 
